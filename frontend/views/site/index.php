86db6a33147f1b8060b5bed94ca3040bb07672fe
Nika-Hanover <kaleo@i.ua>
2020-06-08 11:12:42 +0300

    Add query to show categories name

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -6,6 +6,7 @@ use task_force\ex\CallNameException;
 use task_force\models\sql_insert;
 use frontend\models\Tasks;
 use frontend\models\Users;
+use frontend\models\categories;
 require_once '../../vendor/autoload.php';
 
 /* @var $this yii\web\View */
@@ -15,8 +16,15 @@ $this->title = 'Task Force';
 <div class="site-index" style="display:block; min-height: 63vh">
     <?php
         $task = new Tasks();
-        $query = $task->find()->select(['t.title','t.description','t.budget','t.dt_add','t.deadline','c.*'])->from('tasks t')->join('INNER JOIN', 'categories c', 't.idcategory = c.id')->orderBy(['c.id' => SORT_ASC])->limit(10);;
+        $query = $task->find()->select(['t.title','t.description','t.budget','t.dt_add','t.deadline','c.*'])->from(['tasks t','categories c'])->where('t.idcategory = c.id')->orderBy(['c.id' => SORT_ASC])->limit(10);
         $rows = $query->all();
+
+        $categ = new Categories();
+
+        foreach($rows as $row){
+            $i= $row['id'];
+            $catgory[$i] = $categ->find()->select(['c.category'])->from('categories c')->where("c.id = $i")->one();
+        }
     ?>
     <table style='border: solid 1px lightgrey; font-size: 10pt'>
         <h3>Список задач</h3>
@@ -31,7 +39,7 @@ $this->title = 'Task Force';
 
     <?php foreach($rows as $row):?>
         <tr style='border: solid 1px lightgrey;'>
-            <td><?php echo $row['id']?></td>
+            <td><?php echo $catgory[$row['id']]['category']?></td>
             <td><?php echo $row['title']?></td>
             <td><?php echo $row['description']?></td>
             <td><?php echo $row['budget']?></td>
