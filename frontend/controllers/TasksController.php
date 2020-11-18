e5af95b43e06ce0004c9248e1c93fb9b6b583524
Nika-Hanover <kaleo@i.ua>
2020-11-18 15:43:54 +0200

    Updated code structure

--- frontend/controllers/TasksController.php
+++ frontend/controllers/TasksController.php
@@ -4,10 +4,10 @@ namespace frontend\controllers;
 use Yii;
 use yii\helpers\ArrayHelper;
 use yii\web\Controller;
-use frontend\models\src\TasksSearch;
-use frontend\models\categories;
+use frontend\models\Categories;
 use frontend\models\CategoriesFormNew;
 use frontend\models\Tasks;
+use frontend\functions;
 
 class TasksController extends Controller
 {   
@@ -20,7 +20,7 @@ class TasksController extends Controller
             $form_data = Yii::$app->request->post();
             $task_form->load($form_data);
         }
-    
+
         $category = Categories::find()->select(['category', 'id'])->from('categories')->all();
         $categories = (ArrayHelper::map($category, 'id', 'category'));
 
@@ -31,16 +31,73 @@ class TasksController extends Controller
                     'day' => 'За день',
                     'week' => 'За неделю',
                     'month' => 'За месяц'];
-        
-        $search = new TasksSearch('task');
-        $parsed_data = $search->parse_data($form_data['CategoriesFormNew']);
-        $rows = $search->search($parsed_data);
-        $tasks = $search->search($parsed_data);
+
+        $parsed_data = $this->parse_data($form_data['CategoriesFormNew']);
+        $rows = $this->search($parsed_data);
+        $tasks = $this->search($parsed_data);
         return $this->render('/site/tasks', ['categories' => $categories,
                                              'categoryTasks' => $categoryTasks,
                                              'period' => $period,
                                              'task_form' => $task_form,
                                              'tasks' => $tasks]);
+    }
+
+    public function parse_data($form_data){
+
+        if($form_data['category']){
+            $form_data['category'] = implode(",",$form_data['category']);
+        }
+        if($form_data['no_executers']){
+            $form_data['no_executers'] = true;
+        }
+        if($form_data['no_address']){
+            $form_data['no_address'] = true;
+        }
+        if($form_data['period']){
+            switch ($form_data['period']) {
+                case 'day':
+                    $form_data['period'] = date('Y-m-d',time()-86400);
+                    break;
+                case 'week':
+                    $form_data['period'] = date('Y-m-d',time()-86400*7);
+                    break;
+                case 'month':
+                    $form_data['period'] = date('Y-m-d',time()-86400*30);
+                    break;
+                default:
+                    $form_data['period'] = false;
+                    break;
+            }
+        }
+        if($form_data['search']){
+            $form_data['search'] = $form_data['search'];
+        }
+
+        return $form_data;
+    }
+
+    public function search($form_data = null){
+        $query = Tasks::find()->select(['t.title','t.description','t.budget','t.address','t.dt_add','t.deadline','t.idcategory as id'])->from(['tasks t'])->where("current_status = 'new'");
+        //Подключаем переданные фильтры через форму
+        if($form_data['category']){
+            $query = $query->andWhere("t.idcategory in (".$form_data['category'].")");
+        }
+        if($form_data['no_executers']){
+            $query = $query->andWhere("t.id not in (select distinct idtask from responds)");
+        }
+        if($form_data['no_address']){
+            $query = $query->andWhere("t.latitude is null or t.longitude is null");
+        }
+        if($form_data['period']){
+            $query = $query->andWhere("date(dt_add) >= date('".$form_data['period']."')");
+        }
+        if($form_data['search']){
+            $query = $query->andWhere("MATCH(title) AGAINST('*".$form_data['search']."*' in boolean mode)");
+        }
+
+        $query = $query->orderBy(['t.dt_add' => SORT_DESC])->limit(5);
+        $rows = $query->all();
+        return $rows;
     }
 
 }
--- frontend/controllers/UsersController.php
+++ frontend/controllers/UsersController.php
@@ -4,9 +4,11 @@ namespace frontend\controllers;
 use Yii;
 use yii\helpers\ArrayHelper;
 use yii\web\Controller;
-use frontend\models\src\UsersSearch;
-use frontend\models\categories;
+use yii\db\Query;
+use frontend\models\Categories;
 use frontend\models\UsersForm;
+use frontend\models\ExecutersCategory;
+use frontend\models\Users;
 
 class UsersController extends Controller
 {
@@ -31,16 +33,98 @@ class UsersController extends Controller
         $category = Categories::find()->select(['category', 'id'])->from('categories')->all();
         $categories = (ArrayHelper::map($category, 'id', 'category'));
 
-        $search = new UsersSearch('users');
-        $parsed_data = $search->parse_data($form_data['UsersForm']);
-        $users = $search->search($parsed_data);
-        $users_addition = $search->getAddition($users);
+        $parsed_data = $this->parse_data($form_data['UsersForm']);
+        $users = $this->search($parsed_data);
+        $users_addition = $this->getAddition($users);
 
         return $this->render('/site/users',['sortField' => $sortField,
                                             'categories' => $categories,
                                             'users' => $users,
                                             'users_addition' => $users_addition,
                                             'user_form' => $user_form]);
+    }
+
+    //Представляем, данные полученные из формы в удобном виде
+    public function parse_data($form_data){
+        if($form_data['category']){
+            $form_data['category'] = implode(",",$form_data['category']);
+        }
+        return $form_data;
+    }
+
+    //Выводим список исполнителей, которые принадлежат к выбранной категории
+    private function getExequtersCategory($categories=null){
+        $idexecuters = ExecutersCategory::find()->distinct()->select(['idexecuter'])->from('executers_category');
+        if($categories){
+            $idexecuters = $idexecuters->andWhere("idcategory in ($categories)");
+        }
+        $idexecuters = $idexecuters->all();
+
+        //Записываем в масив список всех исполнителей с нужной категорией
+        foreach($idexecuters as $value){
+            $array_execurters[] = $value['idexecuter'];
+        }
+        $array_execurters = implode(",",$array_execurters);
+
+        return $array_execurters;
+    }
+
+    // Выводим список всех исполнителей с их id, fio и датой регистрации с условиями фильтрации
+    public function search($form_data = null){
+        $idexecuters = $this->getExequtersCategory($form_data['category']);
+        $users = (new Query())
+                    ->select(['u.id', 'u.fio', 'u.dt_add', 'u.last_update', 'u.avatar', 'u.about', 'avg(ifnull(f.rate,0)) as rate' , 'COUNT(t.id) as qtask' , 'u.views'])
+                    ->from('users u')
+                    ->leftJoin('feadback f','f.idexecuter = u.id')
+                    ->leftJoin('tasks t','t.idexecuter = u.id')
+                    ->where('u.role = 2')
+                    ->andWhere("u.id in ($idexecuters)");
+        if($form_data['search']){
+            $users = $users->andWhere("MATCH(fio) AGAINST('*".$form_data['search']."*' in boolean mode)");
+        } else {
+            if($form_data['free']){
+                $users = $users->andWhere("NOT EXISTS (SELECT * FROM tasks t WHERE t.idexecuter = u.id AND t.current_status IN ('new','in_progress'))");
+            }
+            if($form_data['online']){
+                $users = $users->andWhere("u.last_update >= date_sub(NOW(),INTERVAL 30 MINUTE)");
+            }
+            if($form_data['feedback']){
+                $users = $users->andWhere("u.id in (select distinct idexecuter from feadback)");
+            }
+            if($form_data['favorite']){
+                $users = $users->andWhere("u.id in (select distinct idexecuter from favorite)");
+            }
+        }
+        $users = $users->groupBy(['u.id','u.fio', 'u.dt_add', 'u.last_update', 'u.avatar', 'u.about', 'u.views']);
+        if($form_data['s'] === 'date'){
+            $users = $users->orderBy(['u.dt_add'=> SORT_DESC]);
+        }
+        if($form_data['s'] === 'rate'){
+            $users = $users->orderBy(['rate'=> SORT_DESC]);
+        }
+        if($form_data['s'] === 'orders'){
+            $users = $users->orderBy(['qtask'=> SORT_DESC]);
+        }
+        if($form_data['s'] === 'favor'){
+            $users = $users->orderBy(['views'=> SORT_DESC]);
+        }
+        $users = $users->limit(5)->all();
+
+        return $users;
+    }
+
+    //Формируем дополнительные данные для ответа
+    public function getAddition($users){
+        // Для каждого исполнителея выводим массив с idcategory работ которые они выполняют
+        foreach($users as $user){
+            $i= $user['id'];
+            $iduser = Users::findOne($i);
+            $idexecuters = $iduser->executersCategories;
+            foreach($idexecuters as $value){
+                $array[$i]['idcategories'][] = $value->idcategory;
+            }
+        }
+        return $array;
     }
 
 }
